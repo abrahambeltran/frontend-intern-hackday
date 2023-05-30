1e237ec92a0751b90a1ed3802710acaf4fce8f52
abrahambeltran <abraham.beltran.2001@gmail.com>
2023-05-30 15:46:41 -0400

    Working search

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import RepoList from "./components/RepoList";
-import NavBar from "./components/NavBar";
 
 function App() {
-  const [repositories, setRepositories] = useState([]);
+  const [searchedOrg, setSearchedOrg] = useState('');
 
-  useEffect(() => {
-    const fetchRepositories = async () => {
-      try {
-        const response = await fetch('https://api.github.com/orgs/Netflix/repos');
-        const data = await response.json();
-        setRepositories(data);
-      } catch (error) {
-        console.error('Error:', error.message);
-      }
-    };
-
-    fetchRepositories();
-  }, []);
+  const handleSearch = orgName => {
+    setSearchedOrg(orgName);
+  };
   return (
     <>
-    <h1>Netflix Repositories</h1>
-    <NavBar/>
-    <RepoList repositories={repositories} />
+    <RepoList orgName={searchedOrg} onSearch={handleSearch}/>
     </>
   );
 }
--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -1,47 +1,116 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import CommitList from "./CommitList";
+import { Octokit } from "@octokit/rest";
 
-function RepoList({ repositories }) {
-  const [selectedRepository, setSelectedRepository] = useState(null);
+const octokit = new Octokit({
+  auth: "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",
+});
+const RepoList = ({ orgName, onSearch }) => {
+  const [repositories, setRepositories] = useState([]);
+  const [selectedRepo, setSelectedRepo] = useState(null);
+  const [commits, setCommits] = useState([]);
 
-  const handleRepositoryClick = async (repository) => {
+  useEffect(() => {
+    fetch(`https://api.github.com/orgs/Netflix/repos`)
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedRepos = data.sort(
+          (a, b) => b.stargazers_count - a.stargazers_count
+        );
+        setRepositories(sortedRepos);
+      })
+      .catch((error) => console.log(error));
+  }, [orgName]);
+
+  const handleRepoClick = (repoName) => {
+    fetch(`https://api.github.com/repos/${orgName}/${repoName}/commits`)
+      .then((response) => response.json())
+      .then((data) => {
+        const sortedCommits = data.sort(
+          (a, b) =>
+            new Date(b.commit.author.date) - new Date(a.commit.author.date)
+        );
+        setCommits(sortedCommits);
+        setSelectedRepo(repoName);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const handleSearch = async (event) => {
+    event.preventDefault();
+    const newOrgName = event.target.elements.orgName.value;
+    onSearch(newOrgName);
+
     try {
-      const response = await fetch(
-        repository.commits_url.replace("{/sha}", "")
+      const { data } = await octokit.repos.listForOrg({
+        org: newOrgName,
+      });
+
+      const sortedRepos = data.sort(
+        (a, b) => b.stargazers_count - a.stargazers_count
       );
-      const data = await response.json();
-      setSelectedRepository({ ...repository, commits: data });
+      setRepositories(sortedRepos);
+      setRepositories(sortedRepos);
     } catch (error) {
-      console.error("Error:", error.message);
+      console.log(error);
     }
   };
 
   return (
     <div>
-      <ul>
-        {repositories.map((repository) => (
-          <a
-            key={repository.id}
-            onClick={() => handleRepositoryClick(repository)}
-          >
-            <h3>{repository.name}</h3>
-            <p>Language: {repository.language}</p>
-            <p>Description: {repository.description}</p>
-            <p>Stars: {repository.stargazers_count}</p>
-            <p>Forks: {repository.forks_count}</p>
-            <p>Date Created: {repository.created_at}</p>
-          </a>
-        ))}
-      </ul>
-      {selectedRepository && <CommitList repository={selectedRepository} />}
+      <div className="flex flex-row justify-evenly">
+        <h1 className="m-6">GitHub Repositories</h1>
+        <form onSubmit={handleSearch} className="m-6 border-2">
+          <input
+            type="text"
+            name="orgName"
+            placeholder="Enter organization name"
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <div className="flex flex-row m-6">
+        {orgName && (
+          <ul className="flex flex-row m-6">
+            {repositories.map((repo) => (
+              <li key={repo.id} onClick={() => handleRepoClick(repo.name)}>
+                <h3>{repo.name}</h3>
+                <p>Language: {repo.language}</p>
+                <p>Description: {repo.description}</p>
+                <p>Stars: {repo.stargazers_count}</p>
+                <p>Forks: {repo.forks_count}</p>
+                <p>Date Created: {repo.created_at}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="flex flex-row m-6">
+        {selectedRepo && (
+          <div className="m-6">
+            <h2>Commits for {selectedRepo}</h2>
+            <ul className="flex flex-row m-6">
+              {commits.map((commit) => (
+                <li key={commit.sha}>
+                  <h4>{commit.commit.message}</h4>
+                  <p>Committer: {commit.commit.author.name}</p>
+                  <p>Commit Hash: {commit.sha}</p>
+                  <p>Date Created: {commit.commit.author.date}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
-}
+};
 
 RepoList.propTypes = {
   repositories: PropTypes.string.isRequired,
+  orgName: PropTypes.string.isRequired,
+  onSearch: PropTypes.func.isRequired,
 };
 
 export default RepoList;
